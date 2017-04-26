12c3d94c2a04e1375a560f6e251a9f9553a55be8
Daniel Pullan <dandandrumsgm@gmail.com>
2017-04-26 11:33:00 +0100

    Tidied up for Space :sheep:

--- core/manage/config.php
+++ core/manage/config.php
@@ -2,15 +2,15 @@
 // Configuration file for Krypton \\
 
 	//Server configuration (Database connection!)
-	$_CONFIG['mysql']['host'] = 'localhost'; //The IP for the datebase server
-	$_CONFIG['mysql']['user'] = 'root'; //The username for the database server
-	$_CONFIG['mysql']['pass'] = '1123'; //The password for the datebase server
-	$_CONFIG['mysql']['db'] = 'Thorium'; //Select the database you want to connect too
+	$_CONFIG['mysql']['host'] = 'insert-host-here'; //The IP for the datebase server
+	$_CONFIG['mysql']['user'] = 'insert-user-here'; //The username for the database server
+	$_CONFIG['mysql']['pass'] = 'insert-password-here'; //The password for the datebase server
+	$_CONFIG['mysql']['db'] = 'insert-database-here'; //Select the database you want to connect too
 	//Site Configuration
 	$_CONFIG['site']['url'] = 'http://localhost'; //Server URL example.com (DOES NOT END WITH /)
-	$_CONFIG['site']['name'] = 'Poole High School'; //Website name, {SITE_NAME}
-	$_CONFIG['site']['template'] = 'Thorium'; //Krypton Template System, Select the Template Directory
-	$_CONFIG['site']['tw'] = 'poole_high'; //Your website Twitter profile link
-	$_CONFIG['site']['fb'] = 'poolehs'; //Your website facebook page link
-	$_CONFIG['site']['twapi'] = 'https://twitter.com/poole_high'; //Your website twitter API
+	$_CONFIG['site']['name'] = 'insert-name-here'; //Website name, {SITE_NAME}
+	$_CONFIG['site']['template'] = 'insert-template-name-here'; //Krypton Template System, Select the Template Directory
+	$_CONFIG['site']['tw'] = 'insert-twitter-profile-here'; //Your website Twitter profile link
+	$_CONFIG['site']['fb'] = 'insert-facebook-profile-here'; //Your website facebook page link
+	$_CONFIG['site']['twapi'] = 'insert-twapi-here'; //Your website twitter API
 ?>
